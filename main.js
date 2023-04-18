11000bf489aa0e474cdbb798c9fa453c986c48aa
Tim Jonsson <4yourztruly@gmail.com>
2023-04-18 11:58:00 +0200

    v1.01

---
+++ main.js
@@ -0,0 +1,44 @@
+/* different ways of reading data from a source */
+
+//promise chaining
+function readFilePromise() {
+        fetch('customers.json')
+        .then(response => {
+
+            if(!response.ok) {
+
+                throw new Error('Fetch error: ' + response.status);
+            }
+            return response.json();
+        })
+
+        .then(customers => {
+
+            generatedHTML(customers);
+        })
+        .catch(error => {
+
+            console.log('Error: ' + error);
+        })
+}
+
+//render HTML
+function generatedHTML(customers) {
+
+    let html = '';
+    for(let customer of customers) {
+
+        html +=`
+             <h3>${customer.firstName} ${customer.lastName}</h3>
+             <p>${customer.firstName} works at ${customer.companyName}.</p>
+        `;
+       
+
+    }
+    html += '<hr>';
+    let customerDiv = document.createElement('div');
+    customerDiv.innerHTML = html;
+    document.querySelector('body').append(customerDiv);
+}
+
+readFilePromise();
